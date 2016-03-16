1f4fa3f03e586352c01e0d98cf05080b5d7c987c
USER <USER@USER-PC>
2016-03-16 16:41:14 +0900

    1

--- projectv1/src/main/java/com/project/v1/HomeController.java
+++ projectv1/src/main/java/com/project/v1/HomeController.java
@@ -1,15 +1,19 @@
 package com.project.v1;
 
+import java.sql.SQLException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.project.v1.test.TestDao;
 
 /**
  * Handles requests for the application home page.
@@ -19,10 +23,13 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	private TestDao testDao;
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+	@RequestMapping(value = "home", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
@@ -32,6 +39,15 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping(value = "test", method = RequestMethod.GET)
+	public String test(Locale locale, Model model) throws SQLException {
+		logger.info("test", locale);
+		
+		System.out.println(testDao.test());
 		
 		return "home";
 	}
